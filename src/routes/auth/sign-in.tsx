2c244fad63e52d1c0951c3d5e457aaee3e8e9a81
Muhammad Irtiza <muhammad.irtiza751@gmail.com>
2025-12-20 22:35:14 +0500

    setup basic signin ui

--- src/routes/auth/sign-in.tsx
+++ src/routes/auth/sign-in.tsx
@@ -1,3 +1,14 @@
+import { Logo } from '@/components/logo'
+import { Button } from '@/components/ui/button'
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+  FieldSeparator,
+} from '@/components/ui/field'
+import { Input } from '@/components/ui/input'
+import { cn } from '@/lib/utils'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/auth/sign-in')({
@@ -5,5 +16,69 @@ export const Route = createFileRoute('/auth/sign-in')({
 })
 
 function RouteComponent() {
-  return <div>Hello "/auth/sign-in"!</div>
+  return (
+    <div className="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <div className={cn('flex flex-col gap-6')}>
+          <form>
+            <FieldGroup>
+              <div className="flex flex-col items-center gap-2 text-center">
+                <a
+                  href="#"
+                  className="flex flex-col items-center gap-2 font-medium"
+                >
+                  <div className="flex size-8 items-center justify-center rounded-md">
+                    {/* <GalleryVerticalEnd className="size-6" /> */}
+                    <Logo size={30} />
+                  </div>
+                  <span className="sr-only">Lanes</span>
+                </a>
+                <h1 className="text-xl font-bold">Welcome to Lanes.</h1>
+                <FieldDescription>
+                  Don&apos;t have an account? <a href="#">Sign up</a>
+                </FieldDescription>
+              </div>
+              <Field>
+                <FieldLabel htmlFor="email">Email</FieldLabel>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  required
+                />
+              </Field>
+              <Field>
+                <FieldLabel htmlFor="password">Password</FieldLabel>
+                <Input
+                  id="password"
+                  type="password"
+                  placeholder="*************"
+                  required
+                />
+              </Field>
+              <Field>
+                <Button type="submit">Login</Button>
+              </Field>
+              <FieldSeparator>Or</FieldSeparator>
+              <Field>
+                <Button variant="outline" type="button">
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                    <path
+                      d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
+                      fill="currentColor"
+                    />
+                  </svg>
+                  Continue with Google
+                </Button>
+              </Field>
+            </FieldGroup>
+          </form>
+          <FieldDescription className="px-6 text-center">
+            By clicking continue, you agree to our{' '}
+            <a href="#">Terms of Service</a> and <a href="#">Privacy Policy</a>.
+          </FieldDescription>
+        </div>
+      </div>
+    </div>
+  )
 }
